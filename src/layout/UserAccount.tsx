f12a615905c61b7500487f8ac00a0e1dd74f3f7a
VIRTUAL\ragini <ragini@virtualemployee.com>
2025-05-29 18:19:16 +0530

    user account page tabbing system and admin panel user details

--- src/layout/UserAccount.tsx
+++ src/layout/UserAccount.tsx
@@ -1,40 +1,76 @@
-import  { useContext } from 'react'
+import  { useContext, useState } from 'react'
 import UserInfoCard from '../components/UserProfile/UserInfoCard'
 import UserAddressCard from '../components/UserProfile/UserAddressCard'
 import { AuthContext } from '../context/AuthContext';
 import UserSubscription from '../components/UserProfile/UserSubscription';
 
 const UserAccount = () => {
+  const authContext = useContext(AuthContext);
+  if (!authContext) {
+    throw new Error("AuthContext must be used within an AuthProvider");
+  }
+  const {  currentUser  } = authContext
 
-      const authContext = useContext(AuthContext);
-    
-        if (!authContext) {
-          throw new Error("AuthContext must be used within an AuthProvider");
-        }
-    
-        const {  currentUser  } = authContext
-    
-        console.log(currentUser,"=====>currentuser")
+  const [activeTab, setActiveTab] = useState("profile");
+
+  const renderTabContent = () => {
+    switch (activeTab) {
+      case "profile":
+        return (
+          <div className="space-y-6">
+            <UserInfoCard isUserAccount={true} currentUser={currentUser} />
+            <UserAddressCard currentUser={currentUser} />
+          </div>
+        );
+      case "app":
+        return <p>app download Tab</p>;
+      case "subscription":
+        return <UserSubscription />;
+      case "emergency":
+        return <p>emergency Tab</p>;
+      case "media":
+        return <p>Screenshot Tab</p>;
+      default:
+        return null;
+    }
+  };
+
   return (
-    <div> 
+    <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] lg:p-6">
+      <div className="mb-5 flex flex-wrap gap-4 border-b pb-3 text-sm font-medium text-gray-600 dark:text-white/70">
+        <button
+          className={`${activeTab === "profile" ? "text-purple-600 border-b-2 border-purple-600" : ""}`}
+          onClick={() => setActiveTab("profile")}
+        >
+          Profile
+        </button>
+        <button
+          className={`${activeTab === "app" ? "text-purple-600 border-b-2 border-purple-600" : ""}`}
+          onClick={() => setActiveTab("app")}
+        >
+          App Download
+        </button>
+        <button
+          className={`${activeTab === "subscription" ? "text-purple-600 border-b-2 border-purple-600" : ""}`}
+          onClick={() => setActiveTab("subscription")}
+        >
+          Subscription
+        </button>
+        <button
+          className={`${activeTab === "emergency" ? "text-purple-600 border-b-2 border-purple-600" : ""}`}
+          onClick={() => setActiveTab("emergency")}
+        >
+          Emergency Contact
+        </button>
+        <button
+          className={`${activeTab === "media" ? "text-purple-600 border-b-2 border-purple-600" : ""}`}
+          onClick={() => setActiveTab("media")}
+        >
+          Screenshot & Video
+        </button>
+      </div>
 
-        {/* <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-2xl font-bold mb-4">User Account</h1>
-            <p className="text-gray-700">This is the user account page.</p>
-        </div> */}
-
-         <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] lg:p-6">
-                <h3 className="mb-5 text-lg font-semibold text-gray-800 dark:text-white/90 lg:mb-7">
-                  Profile
-                </h3>
-                <div className="space-y-6">
-              
-                  <UserInfoCard isUserAccount ={true} currentUser={currentUser} />
-                  <UserAddressCard currentUser={currentUser} />
-                  <UserSubscription />
-                </div>
-              </div>
-         
+      {renderTabContent()}
     </div>
   )
 }
--- src/layout/UserViewEdit.tsx
+++ src/layout/UserViewEdit.tsx
@@ -3,26 +3,38 @@ import UserMetaCard from "../components/UserProfile/UserMetaCard";
 import UserInfoCard from "../components/UserProfile/UserInfoCard";
 import UserAddressCard from "../components/UserProfile/UserAddressCard";
 import PageMeta from "../components/common/PageMeta";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { useParams } from 'react-router-dom';
+import UserSubscription from "../components/UserProfile/UserSubscription";
+import { fetchUserDetailsById } from "../service/user.service";
 
 export default function UserViewEdit() {
 
   const authContext = useContext(AuthContext);
   const { id, mode } = useParams<{ id: string; mode: string }>();
+  const [selectedUser, setSelectedUser] = useState([]);
 
     if (!authContext) {
       throw new Error("AuthContext must be used within an AuthProvider");
     }
 
-    const {  currentUser  } = authContext
+    const {  currentUser  } = authContext;
 
-    console.log(currentUser,"=====>currentuser")
-   
+    const getUserById = async (id: string) => {
+      const response = await fetchUserDetailsById(id);
+      setSelectedUser(response.data.data);
+      console.log("Selected User:", response.data.data);
+      console.log("User details fetched yes:", response);
+      return response;
+    };
 
-// const [userInfo, setUserInfo] = useState(null)
-// { currentUser }: { currentUser: CurrentUser }
+    useEffect(() => {
+      if (id) {
+        getUserById(id);
+      }
+    }, [id]);
+
 
   return (
     <>
@@ -30,15 +42,16 @@ export default function UserViewEdit() {
         title="React.js Profile Dashboard | TailAdmin - Next.js Admin Dashboard Template"
         description="This is React.js Profile Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
       />
-      <PageBreadcrumb pageTitle="View-User" />
+      <PageBreadcrumb pageTitle={`User ${mode}`} />
       <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] lg:p-6">
         {/* <h3 className="mb-5 text-lg font-semibold text-gray-800 dark:text-white/90 lg:mb-7">
           View-User
         </h3> */}
         <div className="space-y-6">
-          <UserMetaCard currentUser={currentUser} />
-          <UserInfoCard currentUser={currentUser} mode={mode} />
-          <UserAddressCard currentUser={currentUser} mode={mode} />
+          <UserMetaCard currentUser={selectedUser} />
+          <UserInfoCard currentUser={selectedUser} mode={mode} />
+          <UserAddressCard currentUser={selectedUser} mode={mode} />
+          <UserSubscription selectedUser={selectedUser} mode={mode} />
         </div>
       </div>
     </>
